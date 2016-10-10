bde33c85b2ee33ba9dd1e57295e55229c81f3acd
Anthony Fortney <afortney86@gmail.com>
2016-10-10 16:43:55 -0500

    finished functions activity

--- js/functions.js
+++ js/functions.js
@@ -1,27 +1,71 @@
 "use strict";
 
-var myNameIs = 'Anthony'; // TODO: Fill in your name here.
+var myNameIs = 'Anthony'; //  Fill in your name here.
 
-// TODO:
-// Create a function called 'sayHello' that takes a parameter 'name'.
+// Create a function called 'sayHello' that takes a parameter 'name'.;
 // When called, the function should log a message that says hello from the passed in name.
 
-// TODO: Call the function 'sayHello' passing the variable 'myNameIs' as a parameter.
+function sayHello(name) {
+    console.log("Hello, " + name + "!");
+
+}
+
+//Call the function 'sayHello' passing the variable 'myNameIs' as a parameter.
+
+sayHello(myNameIs);
 
 // Don't modify the following line
 // It generates a random number between 1 and 100 and stores it in random
+
 var random = Math.floor((Math.random()*100)+1);
 
-// TODO:
+console.log("Random number is " + random)
+
 // Create a function called 'isOdd' that takes a number as a parameter.
 // The function should use the ternary operator to log a message.
 // The log should tell the number passed in and whether it is odd or not.
 
-// TODO: Call the function 'isOdd' passing the variable 'random' as a parameter.
+function isOdd(number){
+    (number % 2 === 0) ? console.log("Number is even") : console.log("Number is odd");
+}
 
-function sayHello(name) {
+
+//Call the function 'isOdd' passing the variable 'random' as a parameter.
+
+isOdd(random);
+
+//Write a function called add(a, b)
+//write subtract(a ,b)
+//write multiply(a, b)
+//write divide(a, b)
+
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a/ b;
+}
+
+//using these functions and only these functions
+
+//write square(number)
+
+function square(number) {
+    return multiply(number, number);
+}
+
+//write sumOfSquares(a, b)
+    //squares a, squares b,
+    //returns the sum of those numbers
+function sumOfSquare(a, b) {
+    return add (square(a), square(b));
+
 
 }
-function isOdd() {
 
-}
