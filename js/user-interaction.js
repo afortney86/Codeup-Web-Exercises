077bb207676287f77507a796fda2e7a2ad40e7c2
Anthony Fortney <afortney86@gmail.com>
2016-10-11 08:23:01 -0500

    finished user-interaction activity

--- js/user-interaction.js
+++ js/user-interaction.js
@@ -2,8 +2,30 @@
 
 // TODO: Ask the user for their name.
 //       Keep asking if an empty input is provided.
+        var userName = "";
+        while (userName.length < 1) {
+            userName = prompt("Welcome! What is your name?")
+        }
+
+
+
 
 // TODO: Show an alert message that welcomes the user based on their input.
 
+    alert ("Hi " + userName + " my name is HAL");
+
 // TODO: Ask the user if they like pizza.
 //       Based on their answer show a creative alert message.
+
+        var likePizza = prompt("Do you like pizza?");
+        switch (likePizza) {
+            case "yes":
+                alert("Awesome! Me too!");
+                break;
+            case "no":
+                alert("Who Doesn't like pizza?")
+        }
+
+
+
+
